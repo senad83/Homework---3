2fd357eb44e4ec4fa10e2e7845fff4a7767f5947
Senad <senadutah@gmail.com>
2020-05-11 04:58:43 -0600

    homework

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-// Assignment Code
+
+
 var generateBtn = document.querySelector("#generate");
-var howLongPasword = prompt("How long pasword?")
-var youWantLowerCaseLetters = confirm("Lower case letters?")
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseLetters = ["A","B","C","D","E","F","G","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCharacters = ["!","@","#","$","%","^","&","*","()"];
 
-  passwordText.value = password;
+
+function getPasswordOptions() {
+    var passwordArr = [];
+  var howLongPassword = prompt("How long pasword?");
+  console.log(howLongPassword);
+  var youWantLowerCaseLetters = confirm("Lower case letters?");
+  console.log(youWantLowerCaseLetters);
+  var youWantUpperCaseLetters = confirm("Upper case letters");
+  console.log(youWantUpperCaseLetters);
+  var youWantSpecialCharacters = confirm("Special characters");
+  console.log(youWantSpecialCharacters);
+  var youWantNumericCharacters = confirm("Numeric characters");
+  console.log(youWantNumericCharacters);
+
+  var optionsArray = [];
+  // conditionals to determine
+if (youWantLowerCaseLetters) {
+  optionsArray.push(lowerCaseLetters)
+}
+if (youWantUpperCaseLetters) {
+  optionsArray.push(upperCaseLetters)
+}
+if (youWantSpecialCharacters) {
+  optionsArray.push(specialCharacters)
+}
+if (youWantNumericCharacters) {
+  optionsArray.push(numbers)
+}
+console.log(optionsArray)
+
+for (var i = 0; i < howLongPassword; i++) {
+    var randomSelection = optionsArray[Math.floor(Math.random() * optionsArray.length)]
 
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
