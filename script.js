46a2a6f1ac1432c14e79076186b32c23d82f0aa8
Senad <senadutah@gmail.com>
2020-05-16 11:38:21 -0600

    Updated version office hours

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 var generateBtn = document.querySelector("#generate");
-
+var passwordText = document.querySelector('#password');
 
 var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseLetters = ["A","B","C","D","E","F","G","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -13,6 +13,13 @@ function getPasswordOptions() {
     var passwordArr = [];
   var howLongPassword = prompt("How long pasword?");
   console.log(howLongPassword);
+  if (howLongPassword < 8){
+    return; 
+  }
+    else if (howLongPassword > 128) {
+      return;
+    }
+
   var youWantLowerCaseLetters = confirm("Lower case letters?");
   console.log(youWantLowerCaseLetters);
   var youWantUpperCaseLetters = confirm("Upper case letters");
@@ -24,22 +31,28 @@ function getPasswordOptions() {
 
   var optionsArray = [];
   // conditionals to determine
-if (youWantLowerCaseLetters) {
-  optionsArray.push(lowerCaseLetters)
-}
-if (youWantUpperCaseLetters) {
-  optionsArray.push(upperCaseLetters)
-}
-if (youWantSpecialCharacters) {
-  optionsArray.push(specialCharacters)
-}
-if (youWantNumericCharacters) {
-  optionsArray.push(numbers)
-}
-console.log(optionsArray)
+  if (youWantLowerCaseLetters) {
+    optionsArray = optionsArray.concat(lowerCaseLetters)
+  }
+  if (youWantUpperCaseLetters) {
+    optionsArray = optionsArray.concat(upperCaseLetters)
+  }
+  if (youWantSpecialCharacters) {
+    optionsArray = optionsArray.concat(specialCharacters)
+  }
+  if (youWantNumericCharacters) {
+    optionsArray = optionsArray.concat(numbers)
+  }
+  console.log(optionsArray)
 
-for (var i = 0; i < howLongPassword; i++) {
-    var randomSelection = optionsArray[Math.floor(Math.random() * optionsArray.length)]
+var password = ""
 
+  for (var i = 0; i < howLongPassword; i++) {
+      var randomSelection = optionsArray[Math.floor(Math.random() * optionsArray.length)]
+  password += randomSelection;
+  }
+  console.log(password);
+  passwordText.value = password;
 }
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", getPasswordOptions);
+
